876ef80dfa62b4acb71f44ea1e7d20161b82c0a8
Vishwah Sivagurunathan (PGT) <c1046213@newcastle.ac.uk>
2022-12-06 15:52:55 +0000

    Implemented Spatial acceleration structure

--- CSC8503CoreClasses/PhysicsSystem.cpp
+++ CSC8503CoreClasses/PhysicsSystem.cpp
@@ -302,6 +302,33 @@ compare the collisions that we absolutely need to.
 */
 void PhysicsSystem::BroadPhase() {
 
+	broadphaseCollisions.clear();
+	QuadTree <GameObject*> tree(Vector2(1024, 1024), 7, 6);
+
+	std::vector <GameObject*>::const_iterator first;
+	std::vector <GameObject*>::const_iterator last;
+	gameWorld.GetObjectIterators(first, last);
+	for (auto i = first; i != last; ++i) {
+		Vector3 halfSizes;
+		if (!(*i)->GetBroadphaseAABB(halfSizes)) {
+			continue;
+		}
+		Vector3 pos = (*i)->GetTransform().GetPosition();
+		tree.Insert(*i, pos, halfSizes);
+		tree.OperateOnContents([&](std::list <QuadTreeEntry <GameObject*>>& data) {
+			CollisionDetection::CollisionInfo info;
+			for (auto i = data.begin(); i != data.end(); ++i) {
+				for (auto j = std::next(i); j != data.end(); ++j) {
+					//is this pair of items already in the collision set -
+						//if the same pair is in another quadtree node together etc
+					info.a = std::min((*i).object, (*j).object);
+					info.b = std::max((*i).object, (*j).object);
+					broadphaseCollisions.insert(info);
+
+				}
+			}
+			});
+	}
 }
 
 /*
@@ -311,6 +338,16 @@ and work out if they are truly colliding, and if so, add them into the main coll
 */
 void PhysicsSystem::NarrowPhase() {
 
+	for (std::set <CollisionDetection::CollisionInfo >::iterator
+		i = broadphaseCollisions.begin();
+		i != broadphaseCollisions.end(); ++i) {
+		CollisionDetection::CollisionInfo info = *i;
+		if (CollisionDetection::ObjectIntersection(info.a, info.b, info)) {
+			info.framesLeft = numCollisionFrames;
+			ImpulseResolveCollision(*info.a, *info.b, info.point);
+			allCollisions.insert(info); // insert into our main set
+		}
+	}
 }
 
 /*
